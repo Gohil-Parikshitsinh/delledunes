c1998abb7f6c22b762f2119a9ee0079bf992986a
Gohil-Parikshitsinh <parikshitsinhgohil03@gmail.com>
2026-02-15 14:51:30 +0530

    Basic Structure - Backend

---
+++ server/server.js
@@ -0,0 +1,34 @@
+import cookieParser from 'cookie-parser';
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import connectDB from './config/db.js';
+import connectCloudinary from './config/cloudinary.js';
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+await connectDB()
+await connectCloudinary()
+
+const allowedOrigins = ['http://localhost:5173']
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true
+  }));
+  
+
+app.get('/',(req, res)=>res.send("API is working"))
+
+app.listen(port,()=>{
+    console.log(`server is running on http://localhost:${port}`);  
+})
