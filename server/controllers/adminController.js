901407608fb4be22e19e410566644f7b9be77fa2
Gohil-Parikshitsinh <parikshitsinhgohil03@gmail.com>
2026-02-22 16:33:01 +0530

    Updated admin dashborad routes and controller

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -1,4 +1,18 @@
 
 export const getDashboardStats = async (req,res) => {}
+
 export const getInventoryStats = async (req,res) => {}
+export const getLowStockProducts = async (req,res) => {}
+
 export const getReports = async (req,res) => {}
+export const getSalesReports = async (req,res) => {}
+export const getUserReports = async (req,res) => {}
+export const getTopProducts = async (req,res) => {}
+
+export const getRevenueAnalytics = async (req,res) => {}
+export const getMonthlyRevenue = async (req,res) => {}
+
+export const getPredictoinOverview = async (req,res) => {}
+export const getSalesPrediction = async (req,res) => {}
+export const getDemandPrediction = async (req,res) => {}
+export const getUserChurnPrediction = async (req,res) => {}
--- server/routes/adminRoute.js
+++ server/routes/adminRoute.js
@@ -1,12 +1,28 @@
 import express from "express";
 import authUser from "../middlewares/userAuth.js";
 import adminAuth from "../middlewares/adminAuth.js";
-import { getDashboardStats, getInventoryStats, getReports } from "../controllers/adminController.js";
+import { getDashboardStats, getDemandPrediction, getInventoryStats, getLowStockProducts, getMonthlyRevenue, getPredictoinOverview, getReports, getRevenueAnalytics, getSalesPrediction, getSalesReports, getTopProducts, getUserChurnPrediction, getUserReports } from "../controllers/adminController.js";
 
 const adminRouter = express.Router();
 
 adminRouter.get('/dashboard',authUser, adminAuth, getDashboardStats)
+
 adminRouter.get('/inventory',authUser, adminAuth, getInventoryStats)
+adminRouter.get('/inventory/low-stock',authUser, adminAuth, getLowStockProducts)
+
 adminRouter.get('/reports',authUser, adminAuth, getReports)
+adminRouter.get('/reports/sales',authUser, adminAuth, getSalesReports)
+adminRouter.get('/reports/users',authUser, adminAuth, getUserReports)
+adminRouter.get('/reports/products',authUser, adminAuth, getTopProducts)
+
+adminRouter.get('/analytics/revenue',authUser, adminAuth, getRevenueAnalytics)
+adminRouter.get('/analytics/monthly',authUser, adminAuth, getMonthlyRevenue)
+
+adminRouter.get('/predictions',authUser, adminAuth, getPredictoinOverview)
+adminRouter.get('/predictions/sales',authUser, adminAuth, getSalesPrediction)
+adminRouter.get('/predictions/demand',authUser, adminAuth, getDemandPrediction)
+adminRouter.get('/predictions/churn',authUser, adminAuth, getUserChurnPrediction)
+
+
 
 export default adminRouter;
